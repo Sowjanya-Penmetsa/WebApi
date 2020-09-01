df4f5a8bf9efad725359873bdc8cca2b80296fb3
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2020-09-01 14:27:53 +0200

    JavaScript and WebApi lab

---
+++ Javascript and Web Api/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Javascript_and_Web_Api.Models
+{
+    public class Product
+    {
+       
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public double Price { get; set; }
+
+        
+    }
+}
